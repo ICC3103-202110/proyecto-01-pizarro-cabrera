3e4e76668aa4fbb719a2d2771720e81d18b5dd57
Alonsovpm <avpizarro@miuandes.cl>
2021-04-18 13:23:34 -0400

    cambios clase card y coin

--- card.py
+++ card.py
@@ -2,13 +2,32 @@ class Card:
 
     #Constructor
     def __init__(self, name):
-        self.__card = name
+        self.__name = name
+        self.discovered = False
+        self.hidden = True
+
 
     #Getters and Setters
     
     @property
-    def deck(self):
-        return self.__card
+    def name(self):
+        return self.__name
+
+    @property
+    def discovered(self):
+        return self.discovered
+
+    @property
+    def hidden(self):
+        return self.hidden
+
+    @discovered.setter
+    def discovered(self, discovered):
+        self.__discovered = discovered
+
+    @hidden.setter
+    def hidden(self, hidden):
+        self.__hidden = hidden
 
     #Methods
 
--- coin.py
+++ coin.py
@@ -1,11 +1,32 @@
 class Coin:
 
+    #Constants
+    singleCoin= 1
+
     #Constructor
-    def __init__(self, coin):
-        self.__coin = []
+    def __init__(self, quantity):
+        self.__quantity = quantity
 
     #Getters and Setters
     
     @property
-    def deck(self):
-        return self.__coin
+    def coin(self):
+        return self.__quantity
+
+    @value.setter
+    def quantity(self,quantity)
+        if quantity < 0:
+            self.__quantity = 0
+        else:
+            self.__quantity = quantity
+
+    #Methods
+
+    def add_coin(self):
+        self.__quantity += singleCoin
+
+    def substract_coin(self):
+        self.__quantity -= singleCoin
+
+
+    
