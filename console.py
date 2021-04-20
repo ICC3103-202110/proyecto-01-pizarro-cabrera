076de50fe6da0fbede163844cb9a98f5ed84ca70
Alonsovpm <avpizarro@miuandes.cl>
2021-04-20 12:18:17 -0400

    Pregunta por numero de jugadores

--- console.py
+++ console.py
@@ -3,3 +3,6 @@ class Console:
     @staticmethod
     def get_str_input_with_args(message, args):
         return input(message.format(*args))
+
+    def get_int_input(message):
+        return int(input(message))
--- main.py
+++ main.py
@@ -4,23 +4,30 @@ from console import Console
 
 
 class Game:
-    MAX_NUMBER_OF_PLAYERS = 4
+    NUMBER_OF_PLAYERS = 0
     __players = []
     __board = None
     __current_player = None 
 
     @classmethod
     def play(cls):
+        cls.__set_number_of_players()
         cls.__set_players()
 
     @classmethod
     def __set_players(cls):
-        for i in range(1, cls.MAX_NUMBER_OF_PLAYERS + 1):
+        for i in range(1, cls.NUMBER_OF_PLAYERS + 1):
             name = Console.get_str_input_with_args(
                 'Please enter player\'s {} name: ', [i]
             )
             cls.__players.append(Player(name, i))
         cls.__current_player = cls.__players[0]
+
+    @classmethod
+    def __set_number_of_players(cls):
+        while cls.NUMBER_OF_PLAYERS>4 or cls.NUMBER_OF_PLAYERS<3:
+            number = Console.get_int_input("How many players wish to play(Min=3,Max=4): ")
+            cls.NUMBER_OF_PLAYERS = number
 
 
 
